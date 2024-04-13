78653dc17b4955343e1e3aa36c88e5211a6c61e3
Jakieee31 <166881757+Jakieee31@users.noreply.github.com>
2024-04-13 11:22:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from youtube_transcript_api import YouTubeTranscriptApi
 from gtts import gTTS
 import pygame
+from textsum.summarize import Summarizer
 
 
 def text_to_speech(text):
@@ -19,18 +20,18 @@ def main():
     video_url = st.text_input("Enter YouTube video URL:")
 
     
-    if st.button("Get Transcript"):
+    if st.button("Get summary"):
         if video_url:
             try:
                 video_id = video_url.split("v=")[1] 
                 transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
                 transcript_text = ' '.join([t['text'] for t in transcript_list])
                 st.write("Transcript:")
-                st.write(transcript_text)
-                st.write("Reading Transcript:")
-                text_to_speech(transcript_text)
+                summRY = summarizer.summarize_string(transcript_text)
+                print(f'{summRY}')
+                
             except Exception as e:
-                st.write("Error occurred:", Fix it)
+                st.write("Error occurred:Fix it")
         else:
             st.write("Please enter a valid YouTube video URL.")
 
